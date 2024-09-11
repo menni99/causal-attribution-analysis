b9075742407550edad112c6750296aa89dd5a538
“juanpi19” <juanpablacho19@gmail.com>
2024-09-11 16:05:14 -0700

    commit changes

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -27,39 +27,8 @@ Example of function:
 '''
 
 import pandas as pd
-import numpy as np
-from pathlib import Path
-
 
 # ----------------------------------------------------- 1
-# def load_data() -> dict:
-#     '''
-#     ToDo: 
-#         1. Write docstring
-#         2. host data somwhere so we can pull from a centralized place
-#     '''
-#     data_files = {
-#         'olist_customers_df': 'causal-attribution-analysis/data/raw/olist_customers_dataset.csv',
-#         'olist_geolocation_df': 'causal-attribution-analysis/data/raw/olist_geolocation_dataset.csv',
-#         'olist_order_items_df': 'causal-attribution-analysis/data/raw/olist_order_items_dataset.csv',
-#         'olist_order_payments_df': 'causal-attribution-analysis/data/raw/olist_order_payments_dataset.csv',
-#         'olist_order_reviews_df': 'causal-attribution-analysis/data/raw/olist_order_reviews_dataset.csv',
-#         'olist_orders_df': 'causal-attribution-analysis/data/raw/olist_orders_dataset.csv',
-#         'olist_products_df': 'causal-attribution-analysis/data/raw/olist_products_dataset.csv',
-#         'olist_sellers_df': 'causal-attribution-analysis/data/raw/olist_sellers_dataset.csv',
-#         'product_category_name_translation_df': 'causal-attribution-analysis/data/raw/product_category_name_translation.csv',
-        
-#         # Marketing data
-#         'olist_closed_deals_df': 'causal-attribution-analysis/data/raw/marketing_data/olist_closed_deals_dataset.csv',
-#         'olist_marketing_qualified_leads_df': 'causal-attribution-analysis/data/raw/marketing_data/olist_marketing_qualified_leads_dataset.csv'
-
-#     }
-
-#     dataframes = {name: pd.read_csv(path) for name, path in data_files.items()}
-#     return dataframes
-
-
-
 
 def load_data() -> dict:
     '''
@@ -94,4 +63,3 @@ def load_data() -> dict:
 
 
 
-# ----------------------------------------------------- 2
